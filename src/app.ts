5230415454466703147597c5356d6f4f92a24abc
Gabriel Maciel <gabrielc.maciel42@gmail.com>
2023-08-10 13:52:04 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import "express-async-errors";
 import announcementRoutes from "./routes/announcements.routes";
 import loginRoutes from "./routes/login.routes";
 import userRoutes from "./routes/users.routes";
+const cors = require('cors'); 
 
 const app: Application = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use("/login", loginRoutes);
-app.use("/announcements", announcementRoutes);
 app.use("/users", userRoutes);
 export default app;
